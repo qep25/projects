df634f14c80b02ce60b1b49042f9c42e672c8e31
qep25 <106804276+qep25@users.noreply.github.com>
2025-01-18 13:14:34 +0800

    Add files via upload

---
+++ cosrx/con.php
@@ -0,0 +1,7 @@
+  <?php                    $hostname = "localhost:3307";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "cosrx";
+                    
+                    $connect = mysqli_connect($hostname,$username,$password,$dbname) OR DIE ("Connection failed!");
+					?>
