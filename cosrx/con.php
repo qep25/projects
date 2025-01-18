b1e2251c48f1938ecd106c437a40f026b0826681
qep25 <106804276+qep25@users.noreply.github.com>
2025-01-18 13:23:47 +0800

    Update con.php

--- cosrx/con.php
+++ cosrx/con.php
@@ -1,7 +1,1 @@
-  <?php                    $hostname = "localhost:3307";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "cosrx";
-                    
-                    $connect = mysqli_connect($hostname,$username,$password,$dbname) OR DIE ("Connection failed!");
-					?>
+
